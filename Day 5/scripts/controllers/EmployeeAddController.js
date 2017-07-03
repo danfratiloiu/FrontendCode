26ad9c9d201c4e8ebb302ef738b29e423b83d76d
Dan.Fratiloiu <fratiloiu.dan@yahoo.com>
2017-07-03 17:42:13 +0300

    Am ajuns la 11

--- Day 5/scripts/controllers/EmployeeAddController.js
+++ Day 5/scripts/controllers/EmployeeAddController.js
@@ -1,11 +1,70 @@
-hrApp.controller('EmployeeAddController', ['$scope', '$http', '$location', '$commonResourcesFactory',
-    function($scope, $http, $location, $commonResourcesFactory) {
+hrApp.controller('EmployeeAddController', ['$scope', '$http', '$location', 'CommonResourcesFactory', 'EmployeeService', 'DepartmentService', 'JobService', 'ManagerService',
+    function($scope, $http, $location, CommonResourcesFactory, EmployeeService, DepartmentService, JobService, ManagerService) {
         $scope.employee = {};
         $scope.requiredErrorMessage = "Please fill out this form!";
-        $scope.patternDateNotRespectedMessage = "The date format should be yyyy-mm-dd";
+        $scope.patternDateNotRespectedMessage = "The date format should be YYYY-MM-DD";
         $scope.patternCommisionNotRespectedMessage = "Commission should be in the format 0.XX";
 
         //TODO #HR1
+        $scope.employees=[];
+        $scope.managers=[];
+        $scope.departments=[];
+        $scope.jobs=[];
+        var testManager=[];
+
+        /*$scope.departments=DepartmentService.findAllDepartments();
+        $scope.jobs=JobService.findAllJobs();*/
+        EmployeeService.findById()
+            .then(function (res)
+                {
+                    $scope.employee=res.data;
+                },
+                function(err) {
+                    console.log("Error at employee service: " + err);
+                }
+            );
+
+
+
+
+        DepartmentService.findAllDepartments()
+            .then(function (res)
+                {
+                    $scope.departments=res.data;
+                },
+                function(err) {
+                console.log("Error at departments service: " + err);
+                }
+            );
+        JobService.findAllJobs()
+            .then(function (res)
+                {
+                    $scope.jobs=res.data;
+                },
+                function(err) {
+                    console.log("Error at jobs service: " + err);
+                }
+            );
+        ManagerService.findAllManagers()
+            .then(function (res)
+                {
+                    $scope.employees=res.data;
+                    for(i in $scope.employees) {
+                        if (($scope.employees[i].managerId != null) && (testManager.indexOf($scope.employees[i].managerId.employeeId) == -1)) {
+                            testManager.push($scope.employees[i].managerId.employeeId);
+                        }
+                    }
+                    for(i in $scope.employees){
+                        if( testManager.indexOf($scope.employees[i].employeeId) != -1){
+                            $scope.managers.push($scope.employees[i]);
+                        }
+                    }
+
+                },
+                function(err) {
+                    console.log("Error at managers service: " + err);
+                }
+            );
 
         /**
          * Reset form
@@ -19,7 +78,7 @@ hrApp.controller('EmployeeAddController', ['$scope', '$http', '$location', '$com
          * @param addEmployee - employee to be persisted
          */
         $scope.create = function (addEmployee) {
-            $http({url: $commonResourcesFactory.addEmployeeUrl, method: 'POST', data: addEmployee})
+            $http({url: CommonResourcesFactory.addEmployeeUrl, method: 'POST', data: addEmployee})
                 .success(function (data) {
                     $scope.employee = data;
                     $location.url('/employeeView/' + $scope.employee.employeeId);
--- Day 5/scripts/controllers/EmployeeEditController.js
+++ Day 5/scripts/controllers/EmployeeEditController.js
@@ -1,10 +1,60 @@
-hrApp.controller('EmployeeEditController', ['$scope', '$http', '$routeParams', '$location', 'commonResourcesFactory',
-    function ($scope, $http, $routeParams, $location, commonResourcesFactory) {
+hrApp.controller('EmployeeEditController', ['$scope', '$routeParams', '$http', '$location', 'CommonResourcesFactory', 'EmployeeService', 'DepartmentService', 'JobService', 'ManagerService',
+    function($scope, $routeParams, $http, $location, CommonResourcesFactory, EmployeeService, DepartmentService, JobService, ManagerService) {
+        $scope.employee = {};
         $scope.requiredErrorMessage = "Please fill out this form!";
-        $scope.patternDateNotRespectedMessage = "The date format should be yyyy-mm-dd";
+        $scope.patternDateNotRespectedMessage = "The date format should be YYYY-MM-DD";
         $scope.patternCommisionNotRespectedMessage = "Commission should be in the format 0.XX";
 
         //TODO #HR5
+
+        $scope.employees=[];
+        $scope.managers=[];
+        $scope.departments=[];
+        $scope.jobs=[];
+
+        /*$scope.departments=DepartmentService.findAllDepartments();
+         $scope.jobs=JobService.findAllJobs();*/
+        EmployeeService.findById($routeParams.employeeId)
+            .then(function (res)
+                {
+                    $scope.employee=res.data;
+                },
+                function(err) {
+                    console.log("Error at employee service: " + err);
+                }
+            );
+
+
+
+
+        DepartmentService.findAllDepartments()
+            .then(function (res)
+                {
+                    $scope.departments=res.data;
+                },
+                function(err) {
+                    console.log("Error at departments service: " + err);
+                }
+            );
+        JobService.findAllJobs()
+            .then(function (res)
+                {
+                    $scope.jobs=res.data;
+                },
+                function(err) {
+                    console.log("Error at jobs service: " + err);
+                }
+            );
+        ManagerService.findAllManagers()
+            .then(function (res)
+                {
+                    $scope.employees=res.data;
+
+                },
+                function(err) {
+                    console.log("Error at managers service: " + err);
+                }
+            );
 
         /**
          * Reset form
@@ -18,7 +68,7 @@ hrApp.controller('EmployeeEditController', ['$scope', '$http', '$routeParams', '
          * @param addEmployee - employee to be persisted
          */
         $scope.create = function (addEmployee) {
-            $http({url: commonResourcesFactory.editEmployeeUrl, method: 'PUT', data: addEmployee})
+            $http({url: CommonResourcesFactory.editEmployeeUrl, method: 'PUT', data: addEmployee})
                 .success(function (data) {
                     $scope.employee = data;
                     $location.url('/employeeView/' + $scope.employee.employeeId);
